536ba6c58faad2ea9437a5ff3d50517b60262393
levin-mutai <levinmutai2@gmail.com>
2023-01-13 11:53:45 +0300

    added create folder function

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import os
+from colorama import init, Fore
+from termcolor import colored
+
+init()
+
+
+def createFolder(dir: str):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+            print(colored(f"Directory {dir} created", "green"))
+        else:
+            raise OSError
+    except OSError:
+        print(colored(f"Error: Creating directory {dir}. ", "red"))
+
+
+createFolder("/home/sav4ner/dev/fastapi-cli")
