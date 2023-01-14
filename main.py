0fc3dfa8b0917ab9161e586e6764ec5a735525a6
levin-mutai <levinmutai2@gmail.com>
2023-01-14 04:31:52 +0300

    refined create folder async function

---
+++ main.py
@@ -0,0 +1,5 @@
+import asyncio
+
+# To help in debuging and printing errors faced during development.
+PYTHONASYNCIODEBUG = 1
+PYTHONTRACEMALOC = 1
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,22 @@
 import os
+import asyncio
 from colorama import init, Fore
 from termcolor import colored
 
 init()
 
 
-def createFolder(dir: str):
+async def createFolder(dir: str) -> bool:
+    """A coroutine to help create a folder by first checking if the folder already exists and
+    if not it creates it and returns True. If it fails it returns false and araises an OSError"""
     try:
         if not os.path.exists(dir):
-            os.makedirs(dir)
+            await os.makedirs(dir)
             print(colored(f"Directory {dir} created", "green"))
+            return True
         else:
             raise OSError
+
     except OSError:
         print(colored(f"Error: Creating directory {dir}. ", "red"))
+        return False
