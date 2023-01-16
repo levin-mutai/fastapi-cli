26640216773f9d81ecc72c40671b0738a9a7b5f0
levin-mutai <levinmutai2@gmail.com>
2023-01-16 20:09:56 +0300

    made some corrections on createFolder function

--- src/utils.py
+++ src/utils.py
@@ -6,32 +6,33 @@ from termcolor import colored
 init()
 
 
-async def createFolder(dir: str) -> bool:
+def createFolder(dir: str) -> None:
     """A async function to help create a folder by first checking if the folder already exists and
     if not it creates it and returns True. If it fails it returns false and araises an OSError"""
     try:
         if not os.path.exists(dir):
-            await os.makedirs(dir)
+            os.makedirs(dir)
             print(colored(f"Directory {dir} created", "green"))
-            return True
+
         else:
             raise OSError
 
     except OSError:
-        print(colored(f"Error: Creating directory {dir}. ", "red"))
-        return False
+        print(colored(f"Error: directory {dir} already exists. ", "red"))
 
     except Exception as err:
         print(colored(f"Error: Creating directory {dir} due to {err!r}", "red"))
-        return False
 
 
-def check_os():
+def check_os() -> str:
     """used to check the type of os being used"""
     return platform.system()
 
 
-def pwd():
+def pwd() -> str:
     """used to check the current working directory"""
     print(os.getcwd())
     return os.getcwd()
+
+
+createFolder("./test")
